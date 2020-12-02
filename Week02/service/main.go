1ff6295a650d93f9f24f9e9ce8246751a1eed969
gaoyixian <gaoyixian@elex-tech.com>
2020-12-02 12:21:05 +0800

    update

--- Week02/service/main.go
+++ Week02/service/main.go
@@ -10,7 +10,7 @@ func QueryUser(ID uint64) (*dao.Users, error, int) {
 	user, err, code := dao.QueryUser(ID)
 	if err != nil {
 		fmt.Printf("original error: %v \n", xerrors.Cause(err))
-		fmt.Printf("stack error: \n %v\n", err)
+		fmt.Printf("stack error: \n%+v\n", err)
 	}
 
 	return user, nil, code
